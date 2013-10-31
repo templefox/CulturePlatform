c7cd0cddf7ac972bdcb69a3ed790a476e0ce3e19
templefox <13524752199@163.com>
2013-10-31 23:10:02 +0800

    添加了actionBarTest

--- gen/com/example/cultureplatform/R.java
+++ gen/com/example/cultureplatform/R.java
@@ -25,8 +25,12 @@ public final class R {
         public static final int init=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080002;
+        public static final int action_settings=0x7f080006;
         public static final int asdf=0x7f080001;
+        public static final int item1=0x7f080005;
+        public static final int menu_account_user_manager=0x7f080003;
+        public static final int menu_nav=0x7f080002;
+        public static final int menu_search=0x7f080004;
         public static final int textView1=0x7f080000;
     }
     public static final class layout {
@@ -34,7 +38,8 @@ public final class R {
         public static final int activity_start=0x7f030001;
     }
     public static final class menu {
-        public static final int start=0x7f070000;
+        public static final int main=0x7f070000;
+        public static final int start=0x7f070001;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
--- src/com/example/cultureplatform/MainActivity.java
+++ src/com/example/cultureplatform/MainActivity.java
@@ -1,10 +1,22 @@
 package com.example.cultureplatform;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.app.ActionBar;
+import android.app.ActionBar.TabListener;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
+import android.app.ActionBar.OnNavigationListener;
+import android.app.ActionBar.Tab;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
 
-public class MainActivity extends Activity{
+public class MainActivity extends Activity implements OnNavigationListener{
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -13,9 +25,89 @@ public class MainActivity extends Activity{
 		setTheme(R.style.ActionBar);
 		setContentView(R.layout.activity_main);
 		
+		actionBarTest();
+		
+	}
+
+	private void actionBarTest() {
 		ActionBar actionBar = getActionBar();
 		actionBar.setTitle("hehe");
+		actionBar.setDisplayHomeAsUpEnabled(true);
+		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
+		//actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+		//¡Ω’ﬂ‘Ò“ª
+		actionBar.setDisplayShowTitleEnabled(false);
 		
+		List<String> list = new ArrayList<String>();
+		list.add("111");
+		list.add("222");
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_dropdown_item_1line, list);
+		actionBar.setListNavigationCallbacks(adapter, this);
+		
+		Tab tab = actionBar.newTab()
+				.setText("HAHA")
+				.setTabListener(new TabListener(){
+					
+					@Override
+					public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+						// TODO Auto-generated method stub
+						
+					}
+					
+					@Override
+					public void onTabSelected(Tab tab, FragmentTransaction ft) {
+						// TODO Auto-generated method stub
+						
+					}
+					
+					@Override
+					public void onTabReselected(Tab tab, FragmentTransaction ft) {
+						// TODO Auto-generated method stub
+						
+					}
+				});
+		
+		actionBar.addTab(tab);
+		
+		tab = actionBar.newTab()
+				.setText("heihei")
+				.setTabListener(new TabListener(){
+					
+					@Override
+					public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+						// TODO Auto-generated method stub
+						
+					}
+					
+					@Override
+					public void onTabSelected(Tab tab, FragmentTransaction ft) {
+						// TODO Auto-generated method stub
+						
+					}
+					
+					@Override
+					public void onTabReselected(Tab tab, FragmentTransaction ft) {
+						// TODO Auto-generated method stub
+						
+					}
+				});
+		
+		actionBar.addTab(tab);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// TODO Auto-generated method stub
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.main, menu);
+		return true;
+		
+	}
+
+	@Override
+	public boolean onNavigationItemSelected(int itemPosition, long itemId) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 
 }
