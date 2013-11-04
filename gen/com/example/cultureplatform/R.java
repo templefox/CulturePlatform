8d3ba55421bc73aa247a439c2f3b1135eb0de6b7
templefox <13524752199@163.com>
2013-11-04 16:26:07 +0800

    添加了tabsTest

--- gen/com/example/cultureplatform/R.java
+++ gen/com/example/cultureplatform/R.java
@@ -25,17 +25,19 @@ public final class R {
         public static final int init=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080006;
-        public static final int asdf=0x7f080001;
-        public static final int item1=0x7f080005;
-        public static final int menu_account_user_manager=0x7f080003;
-        public static final int menu_nav=0x7f080002;
-        public static final int menu_search=0x7f080004;
+        public static final int action_settings=0x7f080007;
+        public static final int asdf=0x7f080002;
+        public static final int item1=0x7f080006;
+        public static final int menu_account_user_manager=0x7f080004;
+        public static final int menu_nav=0x7f080003;
+        public static final int menu_search=0x7f080005;
+        public static final int pager=0x7f080001;
         public static final int textView1=0x7f080000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int activity_start=0x7f030001;
+        public static final int frag_test=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f070000;
--- src/com/example/cultureplatform/MainActivity.java
+++ src/com/example/cultureplatform/MainActivity.java
@@ -3,20 +3,28 @@ package com.example.cultureplatform;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.example.fragment.TestFragment;
+
 import android.app.ActionBar;
 import android.app.ActionBar.TabListener;
-import android.app.Fragment;
 import android.app.FragmentTransaction;
 import android.app.ActionBar.OnNavigationListener;
 import android.app.ActionBar.Tab;
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v4.view.ViewPager.OnPageChangeListener;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 
-public class MainActivity extends Activity implements OnNavigationListener{
+
+public class MainActivity extends FragmentActivity implements OnNavigationListener{
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -25,16 +33,100 @@ public class MainActivity extends Activity implements OnNavigationListener{
 		setTheme(R.style.ActionBar);
 		setContentView(R.layout.activity_main);
 		
-		actionBarTest();
+		//actionBarTest();
+		tabsTest();
+	}
+
+	private void tabsTest() {
+		ActionBar actionBar = getActionBar();
+		ViewPager viewPager = (ViewPager)findViewById(R.id.pager);
 		
+		actionBar.setDisplayHomeAsUpEnabled(false);
+		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+		
+		FragmentPagerAdapter fragmentPagerAdapter = new FragmentPagerAdapter(getSupportFragmentManager()) {
+			
+			@Override
+			public int getCount() {
+				// TODO Auto-generated method stub
+				
+				return 10;
+			}
+			
+			@Override
+			public android.support.v4.app.Fragment getItem(int arg0) {
+				// TODO Auto-generated method stub
+				Fragment fragment = new TestFragment();
+				
+				
+				return fragment;
+			}
+
+			@Override
+			public CharSequence getPageTitle(int position) {
+				// TODO Auto-generated method stub
+				
+				return "TAB"+(position+1);
+			}
+			
+			
+		};
+		
+		viewPager.setAdapter(fragmentPagerAdapter);
+		viewPager.setOnPageChangeListener(new OnPageChangeListener() {
+			
+			@Override
+			public void onPageSelected(int arg0) {
+				// TODO Auto-generated method stub
+				getActionBar().setSelectedNavigationItem(arg0);
+			}
+			
+			@Override
+			public void onPageScrolled(int arg0, float arg1, int arg2) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void onPageScrollStateChanged(int arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		
+		for(int i = 0;i<fragmentPagerAdapter.getCount();i++)
+		{
+			Tab tab = actionBar.newTab();
+			tab.setText(fragmentPagerAdapter.getPageTitle(i)).setTabListener(new TabListener() {
+				
+				@Override
+				public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+					// TODO Auto-generated method stub
+					
+				}
+				
+				@Override
+				public void onTabSelected(Tab tab, FragmentTransaction ft) {
+					// TODO Auto-generated method stub
+					
+				}
+				
+				@Override
+				public void onTabReselected(Tab tab, FragmentTransaction ft) {
+					// TODO Auto-generated method stub
+					
+				}
+			});
+			actionBar.addTab(tab);
+		}
 	}
 
 	private void actionBarTest() {
 		ActionBar actionBar = getActionBar();
 		actionBar.setTitle("hehe");
 		actionBar.setDisplayHomeAsUpEnabled(true);
-		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
-		//actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+		//actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
+		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
 		//Á½ÕßÔñÒ»
 		actionBar.setDisplayShowTitleEnabled(false);
 		
@@ -109,5 +201,6 @@ public class MainActivity extends Activity implements OnNavigationListener{
 		// TODO Auto-generated method stub
 		return false;
 	}
+
 
 }
